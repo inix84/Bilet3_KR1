cf2e90d6344a7f965034b028948cac55dc5e9be3
inix84 <inix84@mail.ru>
2022-11-04 23:28:03 +0500

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        String c = "маринасдалакурсовую";
+        int number = c.length();
+        System.out.println(number);
+            String c1 = c.substring(0,number/2);
+            String c2 = c.substring(number/2,number);
+            System.out.println(c1 + "/" + c2);
+           }
 }
