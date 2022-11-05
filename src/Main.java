3f7a9d9b08ec3e9817bcdadf26d6897fcb162ecf
inix84 <inix84@mail.ru>
2022-11-05 16:03:51 +0500

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         String c = "маринасдалакурсовую";
-        int number = c.length();
-        System.out.println(number);
-            String c1 = c.substring(0,number/2);
-            String c2 = c.substring(number/2,number);
+        int size = c.length();
+        System.out.println(size);
+            String c1 = c.substring(0,size/2);
+            String c2 = c.substring(size/2,size);
             System.out.println(c1 + "/" + c2);
            }
 }
